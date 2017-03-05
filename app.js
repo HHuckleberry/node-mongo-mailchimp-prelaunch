567bb6f09cc327f62d30c8cea0911b9144a082ba
Tim <tm4113@gmail.com>
2017-03-04 23:19:19 -0700

    mailchimp -readme working

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ var express             = require("express"),
     mongoose            = require("mongoose"),
     User                = require("./models/user"),
     passport            = require("passport"),
+    passportLocal       = require("passport-local"),
     passportLM          = require("passport-local-mongoose");
     validator           = require("express-validator"),
-    mailchimpInstance   = '',
-    listUniqueId        = '',
-    mailchimpApiKey     = '';
+    mailchimpInstance   = 'insert your instanceId here',
+    listUniqueId        = 'insert your list id here',
+    mailchimpApiKey     = 'insert your mailchimp api key here';
 
 app.set("view engine", 'ejs');
 //mongodb for desktop Testing
@@ -21,7 +22,6 @@ app.use(validator());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(validator());
 app.use(express.static('public'));
-
 app.use(cookieParser());
 app.use(expressSession({
   secret: 'a9ds8f98087asdytf87t6fagfuasd7zzccv7887',
@@ -42,67 +42,70 @@ app.get('/', function(req, res){
 
 });
 
-app.post('/', function (req, res) {
+app.post('/', function (req, res, next) {
 
   req.assert('firstName', 'Please enter a First Name longer than 2 characters and not more than 15').len(2, 15);
   req.assert('lastName', 'Most people have a last names between 2 and 16 characters long').len(2, 16);
-  req.assert('email', 'valid email required').isEmail();
+  req.assert('email', 'A valid email is required').isEmail();
   req.getValidationResult().then(function(result) {
     if (!result.isEmpty()) {
       var errors = result.array();
       req.session.errors = errors;
       req.session.success = false;
-      res.render('landing', {errors: req.session.errors, success: req.session.success});
-    }
+      res.render('landing',  { errors: req.session.errors,
+                              success: req.session.success,
+                              exists: req.session.exists});
+    } else{
     req.session.success = true;
-    res.render('landing', {errors: req.session.errors, success: req.session.success})
-
-  });
 
 
+    var username  =   req.body.email;
+    User.findOne({username:username}, function(err, user, next){
+      console.log(user);
+      if(user){
+        req.session.exists = true;
+        res.render("landing", { errors: req.session.errors,
+                                success: req.session.success,
+                                exists: req.session.exists});
+      } else if(!user){
+          var email     =  req.body.email,
+              username  =  req.body.email,
+              firstName =  req.body.firstName,
+              lastName  =  req.body.lastName,
+              newUser   =  {username:username, email: email};
+          User.create(newUser, function(err, data, next){
+          if(err){
+            console.log(err);
+            res.redirect('/*');
+          }
 
-  //         var username  =   req.body.email;
-  //         User.findOne({username:username}, function(err, user, next){
-  //           console.log(user);
-  //           if(user){
-  //             res.redirect("/refer")
-  //           } else if(!user){
-  //               var email     =  req.body.email,
-  //                   username  =  req.body.email,
-  //                   firstName =  req.body.firstName,
-  //                   lastName  =  req.body.lastName,
-  //                   newUser   =  {username:username, email: email};
-  //               User.create(newUser, function(err, data, next){
-  //               if(err){
-  //                 console.log(err);
-  //                 res.redirect('/*');
-  //           }
-  //
-  //           request
-  //           .post('https://' + mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members/')
-  //           .set('Content-Type', 'application/json;charset=utf-8')
-  //           .set('Authorization', 'Basic ' + new Buffer('any:' + mailchimpApiKey ).toString('base64'))
-  //           .send({
-  //             'email_address': req.body.email,
-  //             'status': 'subscribed',
-  //             'merge_fields': {
-  //               'FNAME': req.body.firstName,
-  //               'LNAME': req.body.lastName
-  //             }
-  //           })
-  //           .end(function(err, response) {
-  //             if (response.status < 300 || (response.status === 400 && response.body.title === "Member Exists")) {
-  //               res.redirect('/thanks');
-  //             } else {
-  //               res.redirect('/');
-  //             }
-  //         });
-  // });
-  //
-  //           }
-  //         })
+      request
+      .post('https://' + mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members/')
+      .set('Content-Type', 'application/json;charset=utf-8')
+      .set('Authorization', 'Basic ' + new Buffer('any:' + mailchimpApiKey ).toString('base64'))
+      .send({
+        'email_address': req.body.email,
+        'status': 'subscribed',
+        'merge_fields': {
+          'FNAME': req.body.firstName,
+          'LNAME': req.body.lastName
+        }
+      })
+      .end(function(err, response) {
+        if (response.status < 300 || (response.status === 400 && response.body.title === "Member Exists")) {
+            res.render('landing',  { errors: req.session.errors,
+                                    success: req.session.success,
+                                    exists: req.session.exists});
+        } else {
+          res.redirect('/');
+        }
+      });
+        });
 
-
+      }
+    })
+  }
+    });
 
 })
 
--- readme.md
+++ readme.md
@@ -1,13 +1,26 @@
-Example Launch Page -MailChimp-MongoDB-Validation
-================
-What is this?
-  This is an example app to guide other Node.js developers in incorporating mailchimp to their launchpage signup.
+# Node-PreLaunch with Mailchimp
 
-  Planned:
-    incorporating MailChimp
-    use mongodb to track signups
-          - this db could be used down the road to create 'early adopters' and beta users.
-    use validation methods for the signups.
-    error pages
+# What is this?
 
-    future: recaptcha
+This is an example application using Node.js, MongoDB, and MailChimp to create a launchpage with a sign-up sheet.
+
+Use This example to create your own effective launch page and gather interest while your product is still in development.
+
+# Features
+
+- This app using a form to send the contacts information to your predefined Mailchimp list.
+- The form also saves the information to a MongoDB database. This feature exists for two reasons.
+  - Create a  db that has users information that you can use as sample data or to create a beta where they are the users
+  - The app checks to see if they already signed up.
+
+# Requirements
+
+- Node.js
+- MongoDB
+- MailChimp account
+
+# Setup
+
+- Install [Node.js](https://nodejs.org/en/)
+- Install [MongoDB](https://www.mongodb.com/)
+- Signup for [Mailchimp](http://eepurl.com/cCsH6P)
